73c04efd2eebe54792f5edea6e50ace70b2c0a14
Kenneth Ekman <bkekman@gmail.com>
2022-10-16 14:29:22 +0200

    0.2.45

--- src/profile-list-table.tsx
+++ src/profile-list-table.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Profile} from 'simple-family-tree-model'
+import React from "react";
+import { Profile } from "simple-family-tree-model";
 
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
-} from '@tanstack/react-table'
+} from "@tanstack/react-table";
 
 export interface ProfileList {
   profileList: Profile[];
 }
 
-const familyColumnHelper = createColumnHelper<Profile>()
+const familyColumnHelper = createColumnHelper<Profile>();
 
 export function ProfileListTable(props: ProfileList) {
-  const [data, setData] = React.useState(() => [...props.profileList])
+  const [data, setData] = React.useState(() => [...props.profileList]);
 
   const columns = [
     // familyColumnHelper.accessor(row => row.profileId.itemLink, {
@@ -24,41 +24,41 @@ export function ProfileListTable(props: ProfileList) {
     //   header: () => <span>Profile Id</span>,
     //   footer: info => info.column.id,
     // }),
-    familyColumnHelper.accessor('name', {
-      cell: info => info.getValue(),
+    familyColumnHelper.accessor("name", {
+      cell: (info) => info.getValue(),
       header: () => <span>Name</span>,
     }),
-    familyColumnHelper.accessor('birthDate', {
-      cell: info => info.getValue(),
+    familyColumnHelper.accessor("birthDate", {
+      cell: (info) => info.getValue(),
       header: () => <span>Birth Date</span>,
     }),
-    familyColumnHelper.accessor('birthPlace', {
-      cell: info => info.getValue(),
+    familyColumnHelper.accessor("birthPlace", {
+      cell: (info) => info.getValue(),
       header: () => <span>Birth Place</span>,
     }),
-    familyColumnHelper.accessor('deathDate', {
-      cell: info => info.getValue(),
+    familyColumnHelper.accessor("deathDate", {
+      cell: (info) => info.getValue(),
       header: () => <span>Death Date</span>,
     }),
-    familyColumnHelper.accessor('deathPlace', {
-      cell: info => info.getValue(),
+    familyColumnHelper.accessor("deathPlace", {
+      cell: (info) => info.getValue(),
       header: () => <span>Death Place</span>,
     }),
-  ]
+  ];
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  })
+  });
 
   return (
     <div className="p-2">
       <table>
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
+              {headerGroup.headers.map((header) => (
                 <th key={header.id}>
                   {header.isPlaceholder
                     ? null
@@ -72,9 +72,9 @@ export function ProfileListTable(props: ProfileList) {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
+          {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
+              {row.getVisibleCells().map((cell) => (
                 <td key={cell.id}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
@@ -83,9 +83,9 @@ export function ProfileListTable(props: ProfileList) {
           ))}
         </tbody>
         <tfoot>
-          {table.getFooterGroups().map(footerGroup => (
+          {table.getFooterGroups().map((footerGroup) => (
             <tr key={footerGroup.id}>
-              {footerGroup.headers.map(header => (
+              {footerGroup.headers.map((header) => (
                 <th key={header.id}>
                   {header.isPlaceholder
                     ? null
@@ -100,6 +100,5 @@ export function ProfileListTable(props: ProfileList) {
         </tfoot>
       </table>
     </div>
-  )
+  );
 }
-
